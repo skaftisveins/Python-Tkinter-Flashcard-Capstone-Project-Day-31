3ec2fa2b1397fa61aae2c2c28d0e690418df8ac8
skaftisveins <skaftisveins@gmail.com>
2021-04-20 23:54:54 +0000

    Added flip_card function. Added global timer to flip current card after 3 seconds. Some code refactoring.

--- main.py
+++ main.py
@@ -4,18 +4,30 @@ import random
 
 BACKGROUND = "#B1DDC6"
 CANVAS_BACKGROUND = "#FFFFFF"
-UPPER_FONT_LOOK = "Rubik", 40, "italic"
-LOWER_FONT_LOOK = "Rubik", 60, "bold"
+CARD_TITLE_FONT = "Rubik", 40, "italic"
+CARD_WORD_LOOK = "Rubik", 60, "bold"
+
+df = pd.read_csv("data/french_words.csv")
+to_learn = df.to_dict(orient="records")
+current_card = {}
 
 
-# --------------------- GENERATE A RANDOM WORD  ----------------------- #
+def next_card():
+    # data = {row.French: row.English for (index, row) in df.iterrows()}
+    global current_card, TIMER
+    window.after_cancel(TIMER)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    TIMER = window.after(3000, func=flip_card)
 
-def generate_random_word():
-    df = pd.read_csv("data/french_words.csv")
-    data = {row.French: row.English for (index, row) in df.iterrows()}
-    random_french_word = random.choice(list(data.items()))
-    canvas.itemconfig(upper_text, text=random_french_word[0])
-    canvas.itemconfig(lower_text, text=random_french_word[1])
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -23,32 +35,35 @@ window = Tk()
 window.title("Flashcard Capstone Project")
 window.config(padx=50, pady=50, background=BACKGROUND)
 
+TIMER = window.after(3000, func=flip_card)
+
 # Images
 card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
 wrong_image = PhotoImage(file="images/wrong.png")
 right_image = PhotoImage(file="images/right.png")
 
 canvas = Canvas(width=800, height=526,
                 background=BACKGROUND, highlightthickness=0)
-canvas.create_image(400, 263, image=card_front_img)
+card_background = canvas.create_image(400, 263, image=card_front_img)
 canvas.grid(column=0, row=0, columnspan=2)
-upper_text = canvas.create_text(
-    400, 150, text="Title", font=(UPPER_FONT_LOOK))
-lower_text = canvas.create_text(
-    400, 263, text="text", font=(LOWER_FONT_LOOK))
+card_title = canvas.create_text(
+    400, 150, text="", font=CARD_TITLE_FONT)
+card_word = canvas.create_text(
+    400, 263, text="", font=CARD_WORD_LOOK)
 
 # Buttons
 wrong_mark = Button(image=wrong_image, highlightthickness=0,
-                    command=generate_random_word)
+                    command=next_card)
 wrong_mark.grid(column=0, row=1)
 wrong_mark.config(padx=50, pady=50)
 
 right_mark = Button(image=right_image, highlightthickness=0,
-                    command=generate_random_word)
+                    command=next_card)
 right_mark.grid(column=1, row=1)
 right_mark.config(padx=50, pady=50)
 
-generate_random_word()
+next_card()
 
 
 window.mainloop()
